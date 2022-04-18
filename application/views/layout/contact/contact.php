720ae7fc2094073a495cf46578759e937f6befe0
hernanbaigorria <hernanebaigorria@gmail.com>
2022-04-18 17:06:28 -0300

    update

--- application/views/layout/contact/contact.php
+++ application/views/layout/contact/contact.php
@@ -20,6 +20,7 @@
 
         <div class="row-fluid no-padd margin-lg-30t margin-lg-110b margin-sm-20t margin-sm-70b">
             <div class="col-sm-12 col-lg-offset-2 col-lg-8 col-md-offset-1 col-md-10 col-sm-offset-0 col-xs-12 no-padd">
+                <h3>CONTACT US<br><br></h3>
                 <div class="padd-only-xs ">
 
                     <div class="prague-formidable  vc_formidable">
@@ -38,27 +39,22 @@
                                             <div class="row">
                                                 <div class="col-md-6 col-sm-6">
                                                     <div id="frm_field_19_container" class="frm_form_field form-field  frm_required_field frm_none_container">
-
-                                                        <input type="text" id="field_hm4zn" name="item_meta[19]" value=""
-                                                            placeholder="Name" data-reqmsg="This field cannot be blank."
-                                                            data-invmsg="Name is invalid" />
-                                                    </div>
-                                                    <div id="frm_field_20_container" class="frm_form_field form-field  frm_none_container">
-
-                                                        <input type="text" id="field_rposy" name="item_meta[20]" value=""
-                                                            placeholder="Phone" data-invmsg="Phone is invalid" />
-                                                    </div>
-                                                    <div id="frm_field_21_container" class="frm_form_field form-field  frm_required_field frm_none_container">
-
-                                                        <input type="email" id="field_pk5zg" name="item_meta[21]" value=""
-                                                            placeholder="example@mail.com" data-reqmsg="This field cannot be blank."
-                                                            data-invmsg="Email Address is invalid" />
+                                                        <input type="text" placeholder="Name" required />
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6 col-sm-6">
-                                                    <div id="frm_field_22_container" class="frm_form_field form-field  frm_none_container">
-                                                        <textarea name="item_meta[22]" id="field_jkt2l" rows="5"
-                                                            placeholder="Message" data-invmsg="Paragraph Text is invalid"></textarea>
+                                                    <div id="frm_field_19_container" class="frm_form_field form-field  frm_required_field frm_none_container">
+                                                        <input type="text" placeholder="Last Name" required />
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6 col-sm-6">
+                                                    <div id="frm_field_19_container" class="frm_form_field form-field  frm_required_field frm_none_container">
+                                                        <input type="email" placeholder="Email" required />
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6 col-sm-6">
+                                                    <div id="frm_field_19_container" class="frm_form_field form-field  frm_required_field frm_none_container">
+                                                        <input type="text" placeholder="Phone Number" required />
                                                     </div>
                                                 </div>
                                             </div>
